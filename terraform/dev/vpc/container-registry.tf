b1361d11781d0094c45829f562286fa3a76d143f
devopscope <devopscope@gmail.com>
2025-04-05 10:23:31 +0300

    Reduce resources created

--- terraform/dev/vpc/container-registry.tf
+++ terraform/dev/vpc/container-registry.tf
@@ -1,19 +1,19 @@
-locals {
-  repository_list = ["nodejs-app"]
-  repositories    = { for name in local.repository_list : name => name }
-  # env             = "dev"
-}
-
-resource "aws_ecr_repository" "main" {
-
-  for_each = local.repositories
-
-  name                 = "ecr-${var.env}-${each.key}"
-  image_tag_mutability = "MUTABLE"
-  force_delete         = true
-
-  tags = {
-    environment = var.env
-  }
-
-}
+# locals {
+#   repository_list = ["nodejs-app"]
+#   repositories    = { for name in local.repository_list : name => name }
+#   # env             = "dev"
+# }
+#
+# resource "aws_ecr_repository" "main" {
+#
+#   for_each = local.repositories
+#
+#   name                 = "ecr-${var.env}-${each.key}"
+#   image_tag_mutability = "MUTABLE"
+#   force_delete         = true
+#
+#   tags = {
+#     environment = var.env
+#   }
+#
+# }
