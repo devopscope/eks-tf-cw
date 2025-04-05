0fe3556d363ca92017a9006155b5c6a21b4f465b
devopscope <devopscope@gmail.com>
2025-04-05 11:02:53 +0300

    Remove zarchieve

--- zarchieve/0-locals.tf
+++
@@ -1,9 +0,0 @@
-locals {
-  env              = "dev"
-  region           = "us-east-1"
-  zone1            = "us-east-1a"
-  zone2            = "us-east-1b"
-  eks_name         = "demo"
-  eks_version      = "1.30"
-  application_name = "volkan_application_name"
-}
